a23898386a49998a65b7a6a7fad534f017dd3e92
Tom Wilson <wilsonte@umich.edu>
2021-11-28 18:57:30 -0500

    debug shinyFiles

--- shiny/shared/session/modules/widgets/sourceFileInput/sourceFileInput_server.R
+++ shiny/shared/session/modules/widgets/sourceFileInput/sourceFileInput_server.R
@@ -32,7 +32,27 @@ isLaunchPage <- is.null(appName)
 # enable the local file upload input
 #----------------------------------------------------------------------
 handleIncomingSourceFile <- function(file){
-    message('handleIncomingSourceFile')
+    reportProgress('handleIncomingSourceFile')
+    req(file)
+    req(is.list(file))
+
+    str(file)
+
+#     app-server_1              | 'data.frame':       1 obs. of  4 variables:
+# app-server_1              |  $ name    : chr "GarySmith-2299-LK.mdi.package.zip"
+# app-server_1              |  $ size    : int 1503824
+# app-server_1              |  $ type    : chr "application/x-zip-compressed"
+# app-server_1              |  $ datapath: chr "/tmp/RtmpYlwlAf/f16a85b3f9d45278b703288b/0.zip"
+
+# app-server_1              |  $ files:List of 1
+# app-server_1              |   ..$ 0:List of 4
+# app-server_1              |   .. ..$ : chr ""
+# app-server_1              |   .. ..$ : chr "human_embryo"
+# app-server_1              |   .. ..$ : chr "2299-LK"
+# app-server_1              |   .. ..$ : chr "GarySmith-2299-LK.mdi.package.zip"
+# app-server_1              |  $ root : chr "Projects"
+
+
     loadIncomingFile(
         file = file,
         allowedFileTypes = allowedFileTypes,
@@ -52,7 +72,6 @@ observeEvent(input$fileInput, {
 # as needed, enable the server-side file browser
 #----------------------------------------------------------------------
 if(serverEnv$IS_SERVER && isAuthorizedUser()) {
-    message('adding server file button')
     serverFilesButtonServer(
         'serverFileInput', 
         input, 
