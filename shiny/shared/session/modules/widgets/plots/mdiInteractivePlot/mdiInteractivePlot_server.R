f8177f8e9fb2dfe5ca9ca07c729356aabaaf431d
Tom Wilson <wilsonte@umich.edu>
2023-06-24 08:58:55 -0400

    support bit64 in mdiInteractiveplot

--- shiny/shared/session/modules/widgets/plots/mdiInteractivePlot/mdiInteractivePlot_server.R
+++ shiny/shared/session/modules/widgets/plots/mdiInteractivePlot/mdiInteractivePlot_server.R
@@ -111,7 +111,7 @@ pixelToAxis <- function(x, layout, leftI, rightI, dim, lim, invert){
     max <- dim - rightMargin
     if(x < min) x <- min
     if(x > max) x <- max
-    (x - leftMargin) / (dim - totalMargin) * (lim[2] - lim[1]) + lim[1] 
+    (x - leftMargin) / (dim - totalMargin) * as.numeric(lim[2] - lim[1]) + lim[1] # as.numeric accounts for integer64 axes
 }
 pixelToAxes <- function(x, y){
     contents <- contents()
