760f687ade7e343d0e58856d3c3f9bb5420c6d7f
NA <NA>
2023-11-23 19:38:00 +0000

    test commit and push

--- shiny/shared/ui.R
+++ shiny/shared/ui.R
@@ -43,7 +43,8 @@ userLoginTabItem <- tabItem(tabName = "loginTab", tags$div(class = "text-block",
         id = CONSTANTS$apps$loginPage,
         includeMarkdown( uiLaunchHeader ),
         if(serverEnv$IS_GLOBUS) 
-            loginControls('oauth2LoginButton', 'Globus', 'globus-help.md')
+            "DSFASDFASDFA"
+            # loginControls('oauth2LoginButton', 'Globus', 'globus-help.md')
         else if(serverEnv$IS_GOOGLE)
             loginControls('oauth2LoginButton', 'Google', 'google-help.md')
         else if(serverEnv$IS_KEYED) 
